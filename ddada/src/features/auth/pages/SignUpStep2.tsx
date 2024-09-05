82b1d19178c10a74f5946a3d6b39767aed9860eb
jeontaeho <jth121091@gmail.com>
2024-09-05 22:20:06 +0900

    feat: 회원가입2단계 완성

--- ddada/src/features/auth/pages/SignUpStep2.tsx
+++ ddada/src/features/auth/pages/SignUpStep2.tsx
@@ -8,6 +8,9 @@ import {
 } from '@/features/auth/types/SignUpType.ts'
 import PasswordUnVisible from '@/static/imgs/auth/auth_password_unvisible_icon.svg'
 import PasswordVisible from '@/static/imgs/auth/auth_password_visible_icon.svg'
+import CalenderIcon from '@/static/imgs/auth/signup/calender_icon.svg'
+import PassworeMatchChecked from '@/static/imgs/auth/signup/checked_circle_icon.svg'
+
 interface SignUpStep2Props {
   changeViewStep: (viewStep: SignUpStepType) => void
   submitFormData: SignUpFormData
@@ -23,16 +26,61 @@ export default function SignUpStep2({
     register,
     formState: { errors },
     watch,
-    setError,
-    clearErrors,
+    trigger,
   } = useFormContext<SignUpFormData>()
-
+  const email = watch('email')
+  const password = watch('password')
+  const confirmPassword = watch('confirmPassword')
   const nickName = watch('nickname')
   const phoneNumber = watch('phoneNumber')
+  const birthYear = watch('birthYear')
 
   const [passwordVisibility, setPasswordVisibility] = useState<boolean>(false)
   const [passwordConfirmVisibility, setPasswordConfirmVisibility] =
     useState<boolean>(false)
+  const [authNumber, setAuthNumber] = useState<boolean>(false)
+  const [isPasswordMatch, setIsPasswordMatch] = useState<boolean>(false)
+  const [nickNameCheck, setNickNameCheck] = useState<boolean>(false)
+  const [phoneNumberCheck, setPhoneNumberCheck] = useState<boolean>(false)
+  const [isNextStepEnabled, setIsNextStepEnabled] = useState<boolean>(false)
+  useEffect(() => {
+    // birthYear 의 validation 체크가 바로바로안되어 강제로 trigger로 갱신
+    const checkValidation = async () => {
+      await trigger('birthYear')
+
+      if (
+        email &&
+        password &&
+        confirmPassword &&
+        nickName &&
+        phoneNumber &&
+        birthYear &&
+        isPasswordMatch &&
+        nickNameCheck &&
+        phoneNumberCheck &&
+        Object.keys(errors).length === 0
+      ) {
+        console.log('다음단계활성화')
+        setIsNextStepEnabled(true)
+      } else {
+        setIsNextStepEnabled(false)
+      }
+    }
+
+    checkValidation()
+  }, [
+    errors,
+    email,
+    password,
+    confirmPassword,
+    nickName,
+    phoneNumber,
+    birthYear,
+    trigger,
+  ])
+  useEffect(() => {
+    setIsPasswordMatch(password === confirmPassword)
+  }, [password, confirmPassword])
 
   const handleVisibility = () => {
     setPasswordVisibility(!passwordVisibility)
@@ -42,15 +90,27 @@ export default function SignUpStep2({
     setPasswordConfirmVisibility(!passwordConfirmVisibility)
   }
 
-  // todo 백에다가 닉네임 중복체크 api
   const handleCheckNickName = () => {
     console.log('닉네임 중복체크')
+    // 닉네임 중복체크가 됬다고 가정
+    setNickNameCheck(true)
   }
 
-  // 백에다가 전화번호 인증받기
   const handleCheckPhoneNumber = () => {
     console.log('전화번호 인증받기')
+    setAuthNumber(true)
   }
+
+  const handleReCheckPhoneNumber = () => {
+    console.log('전화번호 인증 다시받기')
+  }
+
+  const handleSendAuthNumber = () => {
+    console.log('받은 인증번호 보내기')
+    // 인증하기를 눌러서 인증이 됬다고 가정
+    setPhoneNumberCheck(true)
+  }
+
   const emailRegister = register('email', {
     required: '이메일을 입력해주세요.',
     pattern: {
@@ -58,20 +118,21 @@ export default function SignUpStep2({
       message: '이메일 형식이 올바르지 않습니다',
     },
   })
+
   const passwordRegister = register('password', {
     required: {
       value: true,
       message: '해당 칸이 빈칸입니다.',
     },
   })
+
   const passwordConFirmRegister = register('confirmPassword', {
     required: {
       value: true,
       message: '해당 칸이 빈칸입니다.',
     },
-
     validate: (value) =>
-      value === watch('password') || '비밀번호가 일치하지 않아요를레이이후.',
+      value === watch('password') || '비밀번호가 일치하지 않아요.',
   })
 
   const nicknameRegister = register('nickname', {
@@ -95,20 +156,22 @@ export default function SignUpStep2({
       message: '전화번호 형식을 확인해주세요.',
     },
   })
+
   const birthYearRegister = register('birthYear', {
     required: {
       value: true,
       message: '해당 칸이 빈칸입니다.',
     },
-    // 생년월일을 입력받음
     pattern: {
       value:
         /^(19[0-9][0-9]|20[0-2][0-9]|2030)(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])$/,
       message: '생년월일 형식을 확인해주세요.',
     },
   })
+
   return (
     <>
+      {/* 이메일 입력 */}
       <div className="text-sm">
         <label htmlFor="email">
           <p className="text-[#6B6E78]">이메일</p>
@@ -118,7 +181,7 @@ export default function SignUpStep2({
               id="email"
               placeholder="이메일을 입력해주세요."
               className="w-full focus:outline-none"
-              // eslint-disable-next-line react/jsx-props-no-spreading
+              autoComplete="new-email"
               {...emailRegister}
             />
           </div>
@@ -127,6 +190,8 @@ export default function SignUpStep2({
           )}
         </label>
       </div>
+
+      {/* 비밀번호 입력 */}
       <div className="text-sm">
         <label htmlFor="password">
           <p className="text-[#6B6E78]">비밀번호</p>
@@ -136,10 +201,12 @@ export default function SignUpStep2({
               id="password"
               placeholder="비밀번호를 입력해주세요."
               className="w-full focus:outline-none"
-              // eslint-disable-next-line react/jsx-props-no-spreading
+              autoComplete="new-password"
               {...passwordRegister}
             />
-
+            {password && isPasswordMatch && (
+              <PassworeMatchChecked className="flex-none" />
+            )}
             <button type="button" onClick={handleVisibility} className="ml-2">
               {passwordVisibility ? (
                 <PasswordVisible className="cursor-pointer" />
@@ -153,6 +220,8 @@ export default function SignUpStep2({
           )}
         </label>
       </div>
+
+      {/* 비밀번호 확인 입력 */}
       <div className="text-sm">
         <label htmlFor="confirmPassword">
           <div className="flex items-center border rounded-xl px-4 py-[1.3125rem] focus-within:ring-1 focus-within:ring-[#FCA211]">
@@ -161,10 +230,12 @@ export default function SignUpStep2({
               id="confirmPassword"
               placeholder="비밀번호를 한번 더 입력해주세요."
               className="w-full focus:outline-none"
-              // eslint-disable-next-line react/jsx-props-no-spreading
+              autoComplete="new-password"
               {...passwordConFirmRegister}
             />
-
+            {password && isPasswordMatch && (
+              <PassworeMatchChecked className="flex-none" />
+            )}
             <button
               type="button"
               onClick={handleConfirmVisibility}
@@ -176,6 +247,7 @@ export default function SignUpStep2({
                 <PasswordUnVisible className="cursor-pointer" />
               )}
             </button>
+            {/* 비밀번호가 일치하면 체크 아이콘 표시 */}
           </div>
           {errors.confirmPassword && (
             <p className="text-red-500">{errors.confirmPassword.message}</p>
@@ -183,42 +255,49 @@ export default function SignUpStep2({
         </label>
       </div>
 
-      <div className="text-sm ">
+      {/* 닉네임 입력 */}
+      <div className="text-sm">
         <label htmlFor="nickname">
           <p className="text-[#6B6E78]">닉네임</p>
-          <div className="flex items-center border rounded-xl px-4 py-[1.3125rem] focus-within:ring-1 focus-within:ring-[#FCA211]">
-            {/* <div className="flex items-center border rounded-xl px-4 py-2 h-[3.875rem] focus-within:ring-1 focus-within:ring-[#FCA211]"> */}
-            <input
-              type="text"
-              id="nickname"
-              placeholder="닉네임을 입력해주세요. (최대 20자)"
-              className="w-full focus:outline-none"
-              // eslint-disable-next-line react/jsx-props-no-spreading
-              {...nicknameRegister}
-            />
+          <div className="flex items-center gap-[0.5rem]">
+            <div className="flex-grow flex items-center border rounded-xl px-4 py-[1.3125rem] focus-within:ring-1 focus-within:ring-[#FCA211]">
+              <input
+                type="text"
+                id="nickname"
+                placeholder="닉네임을 입력해주세요. (최대 20자)"
+                className="w-full focus:outline-none"
+                {...nicknameRegister}
+              />
+              {nickName && nickNameCheck && (
+                <PassworeMatchChecked className="flex-none" />
+              )}
+            </div>
+
+            <button
+              type="button"
+              onClick={() => handleCheckNickName()}
+              className={`px-[1.5rem] py-[1.3125rem] rounded-xl ${
+                nickName && !errors.nickname
+                  ? 'bg-[#FCA211] text-white cursor-pointer'
+                  : 'bg-[#E5E5ED] text-[#6B6E78] cursor-not-allowed'
+              }`}
+              disabled={!nickName}
+            >
+              중복체크
+            </button>
           </div>
           {errors.nickname && (
             <p className="text-red-500">{errors.nickname.message}</p>
           )}
         </label>
-        <button
-          type="button"
-          onClick={() => handleCheckNickName()}
-          className={`px-[1.5rem] py-[1.3125rem] rounded-xl ${
-            nickName && !errors.nickname
-              ? 'bg-[#FCA211] text-white cursor-pointer'
-              : 'bg-[#E5E5ED] text-[#6B6E78] cursor-not-allowed'
-          }`}
-          disabled={!nickName}
-        >
-          중복체크
-        </button>
       </div>
+
+      {/* 전화번호 입력 */}
       <div className="text-sm">
         <label htmlFor="phoneNumber">
           <p className="text-[#6B6E78]">휴대폰번호</p>
-          <div className="flex">
-            <select className=" border rounded-xl px-6 py-[1.3125rem] mr-[0.25rem]  focus-within:ring-1 focus-within:ring-[#FCA211]">
+          <div className="flex gap-[0.5rem]">
+            <select className=" border rounded-xl px-6 py-[1.3125rem] focus-within:ring-1 focus-within:ring-[#FCA211]">
               <option>+82</option>
               <option>+81</option>
             </select>
@@ -228,28 +307,69 @@ export default function SignUpStep2({
                 id="phoneNumber"
                 placeholder="숫자만 입력해주세요."
                 className="w-full focus:outline-none"
-                // eslint-disable-next-line react/jsx-props-no-spreading
                 {...phoneNumberRegister}
               />
+              {phoneNumber && phoneNumberCheck && (
+                <PassworeMatchChecked className="flex-none" />
+              )}
             </div>
           </div>
-          {errors.phoneNumber && (
-            <span className="text-red-500">{errors.phoneNumber.message}</span>
-          )}
         </label>
-        <button
-          type="button"
-          onClick={() => handleCheckPhoneNumber()}
-          className={`px-[1.5rem] py-[1.3125rem] rounded-xl ${
-            phoneNumber && !errors.phoneNumber
-              ? 'bg-[#FCA211] text-white cursor-pointer'
-              : 'bg-[#E5E5ED] text-[#6B6E78] cursor-not-allowed'
-          }`}
-          disabled={!phoneNumber}
-        >
-          인증번호받기
-        </button>
+        {!phoneNumberCheck && authNumber && (
+          <>
+            <div className="flex items-center border rounded-xl px-4 py-[1.3125rem] mt-[0.5rem] focus-within:ring-1 focus-within:ring-[#FCA211]">
+              <input
+                type="text"
+                id="authNumber"
+                placeholder="인증번호"
+                className="w-full focus:outline-none"
+              />
+            </div>
+            <div className="mt-[0.5rem] flex gap-[0.25rem] justify-end">
+              <button
+                type="button"
+                onClick={() => handleReCheckPhoneNumber()}
+                className="px-[1.5rem] py-[1.3125rem] rounded-xl border border-[#FCA211] text-[#FCA211]"
+                disabled={!phoneNumber}
+              >
+                인증번호 재전송
+              </button>
+
+              <button
+                type="button"
+                onClick={() => handleSendAuthNumber()}
+                className={`px-[1.5rem] py-[1.3125rem] rounded-xl ${
+                  phoneNumber && !errors.phoneNumber
+                    ? 'bg-[#FCA211] text-white cursor-pointer'
+                    : 'bg-[#E5E5ED] text-[#6B6E78] cursor-not-allowed'
+                }`}
+                disabled={!phoneNumber}
+              >
+                인증하기
+              </button>
+            </div>
+          </>
+        )}
+
+        {!authNumber && (
+          <div className="mt-[0.5rem] flex justify-end">
+            <button
+              type="button"
+              onClick={() => handleCheckPhoneNumber()}
+              className={`px-[1.5rem] py-[1.3125rem] rounded-xl ${
+                phoneNumber && !errors.phoneNumber
+                  ? 'bg-[#FCA211] text-white cursor-pointer'
+                  : 'bg-[#E5E5ED] text-[#6B6E78] cursor-not-allowed'
+              }`}
+              disabled={!phoneNumber || !!errors.phoneNumber}
+            >
+              인증번호받기
+            </button>
+          </div>
+        )}
       </div>
+
+      {/* 생년월일 입력 */}
       <div className="text-sm ">
         <label htmlFor="birthYear">
           <p className="text-[#6B6E78]">생년월일</p>
@@ -259,20 +379,27 @@ export default function SignUpStep2({
               id="birthYear"
               placeholder="연도. 월. 일"
               className="w-full focus:outline-none"
-              // eslint-disable-next-line react/jsx-props-no-spreading
               {...birthYearRegister}
             />
+            <CalenderIcon />
           </div>
           {errors.birthYear && (
             <p className="text-red-500">{errors.birthYear.message}</p>
           )}
         </label>
       </div>
+
       <button
         type="button"
         onClick={() => changeViewStep(SignUpStepType.step3)}
+        className={`py-[1.1875rem] w-full mt-3 rounded-xl ${
+          isNextStepEnabled
+            ? 'bg-[#FCA211] text-white cursor-pointer'
+            : 'bg-[#E5E5ED] text-[#6B6E78] cursor-not-allowed'
+        }`}
+        disabled={!isNextStepEnabled}
       >
-        test{' '}
+        다음단계{' '}
       </button>
     </>
   )
