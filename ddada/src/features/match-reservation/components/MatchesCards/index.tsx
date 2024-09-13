8340def7e70f7e825902c1e211fa1e320a49e74a
전태호 <jht121091@gmail.com>
2024-09-13 14:46:13 +0900

    refactor: 주석추가 및, 글자크기조절

--- ddada/src/features/match-reservation/components/MatchesCards/index.tsx
+++ ddada/src/features/match-reservation/components/MatchesCards/index.tsx
@@ -4,6 +4,7 @@ import ProfessionalIcon from '@/static/imgs/match-reservation/Professional.svg'
 
 export default function MatchesCards() {
   // todo 이제 이걸 mattch 리스트로 받아서 출력하기
+  // todo 여기 전체에 Link 달아서 매치 id를 토해 매치 상세 페이지로 보내기
   const matchType = '혼합복식'
   const matchRankType = '경쟁'
   const matchDay = '2024-09-08'
@@ -14,7 +15,7 @@ export default function MatchesCards() {
   const matchTime = '10:00'
   const matchCourt = '서울 문화체육관'
   const matchAddress = '서울특별시 강남구 역삼동 2-3'
-  const mmr = 'silver'
+  const mmr = '프로페셔널 2'
   const AteamNum = 1
   const BteamNum = 2
 
@@ -50,11 +51,11 @@ export default function MatchesCards() {
             </div>
           </div>
           <div className="flex justify-end w-[6.25rem]">
-            <div className="flex flex-col justify-center gap-2">
+            <div className="flex flex-col justify-center items-center gap-2">
               <div className="w-10 h-10 rounded-[1000px]">
                 <ProfessionalIcon />
               </div>
-              <p>{mmr}</p>
+              <p className="text-xs text-center">{mmr}</p>
             </div>
           </div>
         </div>
