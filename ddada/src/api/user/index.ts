4066c6648479dcb304c58e5b181d3487530f7d28
전태호 <jht121091@gmail.com>
2024-09-24 09:35:17 +0900

    refactor: 주석 제거

--- ddada/src/api/user/index.ts
+++ ddada/src/api/user/index.ts
@@ -7,17 +7,6 @@ const loginUserRole = async () => {
   return userRole
 }
 
-// const kakaoLogout = async () => {
-//   const accessToken = sessionStorage.getItem('accessToken')
-//   headers.append('Authorization', `Bearer ${accessToken}`)
-//   const res = await fetch('https://kauth.kakao.com/oauth/logout', {
-//     method: 'POST',
-//     headers,
-//   })
-//   const data = await res.json()
-//   return data
-// }
-
 const logOut = async () => {
   const accessToken = sessionStorage.getItem('accessToken')
   if (!accessToken) return null
